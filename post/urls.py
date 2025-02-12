9acd0465d4c3dd20bb247667a040adc06d97d522
mari <massimovamariyam@gmail.com>
2025-02-12 07:53:00 +0500

    Initial commit

--- post/urls.py
+++ post/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import post_list, post_detail, post_create, post_delete
 
 urlpatterns = [
+    path('', post_list, name='home'),  # Сделает / главной страницей
     path('posts/', post_list, name='post_list'),
     path('posts/<int:id>/', post_detail, name='post_detail'),
-    path('posts/create/', post_create, name='post_create'),
+    path('posts/new/', post_create, name='post_create'),
     path('posts/<int:id>/delete/', post_delete, name='post_delete'),
 ]
- 
