4a6c99b0b202a122a15879145f0c8076c550c7c1
mari <massimovamariyam@gmail.com>
2025-03-04 19:25:37 +0500

    Исправила, убрала venv

--- hw_2/manage.py
+++ hw_2/manage.py
@@ -1,9 +1,12 @@
-# manage.py (правильная версия)
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cw_2.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hw_2.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -13,6 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
